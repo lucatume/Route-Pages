425f17036454d3f69608fc4cbf0bca4edd434d01
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-08-20 13:32:41 +0200

    wrote working page generation at activation time test

--- tests/acceptance/PageGenerationCest.php
+++ tests/acceptance/PageGenerationCest.php
@@ -1,5 +1,4 @@
 <?php
-use AcceptanceTester;
 
 class PageGenerationCest
 {
@@ -9,23 +8,18 @@ class PageGenerationCest
      */
     public function it_should_create_a_route_page_if_a_persisted_route_meta_info_is_in_the_db(AcceptanceTester $I)
     {
-        /**
-         * Prepare
-         */
-        $routeMeta = mysql_escape_string(serialize(array('route-one' => array('title' => 'Route one', 'permalink' => 'route-one', 'generate' => 'page'))));
-        $data = array('option_name' => RoutePages_GeneratingRoute::OPTION_ID, 'option_value' => $routeMeta, 'autoload' => 'yes');
-        $I->haveInDatabase('rp_options', $data);
-        // on activation and anytime page generation is triggered the plugin will call this method
-        // on this class
-        $pageManager = new RoutePages_PageManager();
-        /**
-         * Execute
-         */
-        $pageManager->createRoutePages();
-        /**
-         * Verify
-         */
+        // Prepare
+        // arrays are stored serialized in wp db
+        $routeMeta = serialize(array('route-one' => array('title' => 'Route one', 'permalink' => 'route-one', 'generate' => 'page')));
+        $I->haveOptionInDatabase(RoutePages_GeneratingRoute::OPTION_ID, $routeMeta);
+
+        // Execute
         $I->loginAsAdmin();
+        $I->amOnPluginsPage();
+        $I->activatePlugin('wp-router');
+        $I->activatePlugin('route-pages');
+
+        // Verify
         $I->amOnPagesPage();
         $I->see('Route one', '.page-title');
     }
